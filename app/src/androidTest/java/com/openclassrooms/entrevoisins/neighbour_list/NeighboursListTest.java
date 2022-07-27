a0df2337ad4bf71b2b0108ae3c37caafaa051424
CCharly93 <charly.chanthavisouk@gmail.com>
2022-07-27 18:45:08 +0200

    Finalisation des tests instrumentalisés.

--- app/src/androidTest/java/com/openclassrooms/entrevoisins/neighbour_list/NeighboursListTest.java
+++ app/src/androidTest/java/com/openclassrooms/entrevoisins/neighbour_list/NeighboursListTest.java
@@ -2,12 +2,15 @@
 package com.openclassrooms.entrevoisins.neighbour_list;
 
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.Espresso.pressBack;
 import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.swipeLeft;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.assertThat;
 import static android.support.test.espresso.matcher.ViewMatchers.hasMinimumChildCount;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static com.openclassrooms.entrevoisins.utils.RecyclerViewItemCountAssertion.withItemCount;
 import static org.hamcrest.core.IsNull.notNullValue;
 
@@ -16,6 +19,9 @@ import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
 import com.openclassrooms.entrevoisins.R;
+import com.openclassrooms.entrevoisins.di.DI;
+import com.openclassrooms.entrevoisins.model.Neighbour;
+import com.openclassrooms.entrevoisins.service.NeighbourApiService;
 import com.openclassrooms.entrevoisins.ui.neighbour_list.ListNeighbourActivity;
 import com.openclassrooms.entrevoisins.utils.DeleteViewAction;
 
@@ -24,6 +30,7 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import java.util.List;
 
 
 /**
@@ -37,6 +44,7 @@ public class NeighboursListTest {
     private static int FIRST_POSITION_ITEM = 0;
 
     private ListNeighbourActivity mActivity;
+    private NeighbourApiService mService;
 
     @Rule
     public ActivityTestRule<ListNeighbourActivity> mActivityRule =
@@ -46,6 +54,7 @@ public class NeighboursListTest {
     public void setUp() {
         mActivity = mActivityRule.getActivity();
         assertThat(mActivity, notNullValue());
+        mService = DI.getNewInstanceApiService();
     }
 
     /**
@@ -84,5 +93,55 @@ public class NeighboursListTest {
         // Then : we check if textView name_text in Profile Activity is displayed
         onView(withId(R.id.details_name))
                 .check(matches(isDisplayed()));
+    }
+
+    /**
+     * When we select an neighbour, the details/profile activity - of this neighbour - is shown
+     */
+    @Test
+    public void nameText_onProfileActivity_isCorrect() {
+        // Pick a neighbour from the service
+        List<Neighbour> neighboursList = mService.getNeighbours();
+        Neighbour aNeighbour = neighboursList.get(FIRST_POSITION_ITEM);
+
+        // Given : correct name in textView from Profile Activity
+        // When : open the details/profile Activity
+        onView(withId(R.id.list_neighbours))
+                .perform(RecyclerViewActions.actionOnItemAtPosition(FIRST_POSITION_ITEM, click()));
+        // Then : we check if text Name displayed in DetailActivity match with neigbour name.
+        onView(withId(R.id.details_name))
+                .check(matches(withText(aNeighbour.getName())));
+    }
+
+    /**
+     * Verify if favorite tab item list shows only favorites neighbours.
+     */
+    @Test
+    public void myFavoritesList_onFavoriteTabItem_showOnlyFavoriteNeighbours() {
+
+        // Given : favorite neighbours and only favorite neighbours in favorite tab item
+
+        // When : add two favorites onClick on the "fav_button" (details/profile activity) from the full list of neighbours
+        onView(withId(R.id.list_neighbours))
+                .perform(RecyclerViewActions.actionOnItemAtPosition(FIRST_POSITION_ITEM, click()));
+        onView(withId(R.id.fav_button))
+                .perform(click());
+
+        pressBack();
+
+        onView(withId(R.id.list_neighbours))
+                .perform(RecyclerViewActions.actionOnItemAtPosition(FIRST_POSITION_ITEM+1, click()));
+        onView(withId(R.id.fav_button))
+                .perform(click());
+
+        pressBack();
+
+        onView(withId(R.id.container)) // container declared in Activity List Neighbour
+                .perform(swipeLeft());
+
+        // Then : Check if the number of items in Favorite list is same as the number neigbours we added.
+        onView(withId(R.id.list_neighbours_favorite))
+                .check(withItemCount(2));
+
     }
 }
